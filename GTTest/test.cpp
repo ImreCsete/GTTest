d49674f15613ad3da4e91dd93bf70c34d8a6750d
ImreCsete <31795867+ImreCsete@users.noreply.github.com>
2020-01-15 10:59:53 +0100

    Added failing unit test.

--- GTTest/test.cpp
+++ GTTest/test.cpp
@@ -28,3 +28,13 @@ TEST_F(CheckoutTests, CanGetTotalForMultipleItems) {
 TEST_F(CheckoutTests, CanAddDiscount) {
     checkOut.addDiscount("a", 3, 2);
 }
+
+TEST_F(CheckoutTests, CanCalculateTotalWithDiscount) {
+    checkOut.addItemPrice("a", 1);
+    checkOut.addDiscount("a", 3, 2);
+    checkOut.addItem("a");
+    checkOut.addItem("a");
+    checkOut.addItem("a");
+    int total = checkOut.calculateTotal();
+    ASSERT_EQ(2, total);
+}
